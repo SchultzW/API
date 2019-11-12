4918c8c19e93458977202e3585eb426159ab5307
Will Schultz <schultzw@my.lanecc.edu>
2019-11-11 19:23:20 -0800

    hooked up to front end

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const feedRoutes = require('./routes/feed');
+
+const app = express();
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+//set header to avoid CORS error
+//client and server can work on differen domains
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.use('/feed', feedRoutes);
+
+app.listen(8080); //in incoming req with feed goes here
+
