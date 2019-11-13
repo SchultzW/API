bdb5420d0c99e832fb9f110c520d4b9ddbe3595d
SchultzW <36714268+SchultzW@users.noreply.github.com>
2019-11-12 16:30:43 -0800

    tweak

--- controllers/feed.js
+++ controllers/feed.js
@@ -21,7 +21,11 @@ exports.getPosts = (req, res, next) => {
     // Create post in db
     res.status(201).json({
       message: 'Post created successfully!',
-      post: { id: new Date().toISOString(), title: title, content: content }
+      post: { id: new Date().toISOString(), 
+        title: title,
+         content: content,
+        creator:{name:'Will'},
+      createdAt: new Date }
     });
   };
   
