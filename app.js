a16e32867a94834c8254a67c1ee9f777f340863c
SchultzW <36714268+SchultzW@users.noreply.github.com>
2019-11-15 16:24:14 -0800

    work

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+const path=require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const feedRoutes = require('./routes/feed');
-
+const mongoose=require('mongoose');
 const app = express();
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
+app.use('/images', express.static(path.join(__dirname, 'images')));
 //set header to avoid CORS error
 //client and server can work on differen domains
 
@@ -19,5 +22,24 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080); //in incoming req with feed goes here
+//error handling middleware
+app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
+
+mongoose
+  .connect(
+    'mongodb+srv://admin:notapassword@cluster0-ghhtq.mongodb.net/messages?retryWrites=true&w=majority'
+  )
+  .then(result => {
+    app.listen(8080);
+  })
+  .catch(err => console.log(err));
+
+
 
--- routes/feed.js
+++ routes/feed.js
@@ -1,6 +1,5 @@
 const express = require('express');
-
-const{body}=require('express-validator/check');
+const { body } = require('express-validator/check');
 
 const feedController = require('../controllers/feed');
 
@@ -10,9 +9,19 @@ const router = express.Router();
 router.get('/posts', feedController.getPosts);
 
 // POST /feed/post
-router.post('/post',[
-    body('title').trim().isLength({min:5}),//validation
-    body('content').trim().isLength({min:5})
-], feedController.createPost);
+router.post(
+  '/post',
+  [
+    body('title')
+      .trim()
+      .isLength({ min: 5 }),
+    body('content')
+      .trim()
+      .isLength({ min: 5 })
+  ],
+  feedController.createPost
+);
+
+router.get('/post/:postId', feedController.getPost);
 
 module.exports = router;
